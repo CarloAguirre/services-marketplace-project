4b2f4fdb0d90a69860b885d3cc23c3e3f1afcc4b
Carlo Aguirre <carlo_aguirre@outlook.cl>
2022-12-03 13:14:47 -0300

    fotter modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ export const Footer = () => {
         <hr className='container' />
         <p className='text-center'>© Cabra del valle 2022-2023 - Todos los derechos reservados </p>
         <div className='d-flex justify-content-center'>
-            <a href="https://www.instagram.com/cabradelvalle/"><i class="fa-brands fa-instagram fa-2x social-links text-dark"></i></a>
-            <a href="https://wa.me/+56997135239"><i class="fa-brands fa-whatsapp fa-2x ms-4 social-links text-dark"></i></a>       
+            <a href=""><i class="fa-brands fa-instagram fa-2x social-links text-dark"></i></a>
+            <a href=""><i class="fa-brands fa-whatsapp fa-2x ms-4 social-links text-dark"></i></a>       
         </div>
         <hr className='mb-0'/>
     </>
